833b9d7f8bb565a8c14eb3cf0d3100a6b07e7d33
WINDOWS 10 <WINDOWS 10@Windows10>
2018-10-03 14:23:02 +0700

    Modul 4

---
+++ src/Hewan/karnivora.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hewan;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class karnivora {
+
+   
+    public static void main(String[] args) {
+        // Cara penulisan Objek: (superclass) variabel = new(subclass)
+        System.out.println("====Hewan Default====");
+        hewan h = new hewan();
+        h.setNama();
+        h.setSuara();
+        h.setBerat();
+        
+        System.out.println("====Hewan Kucing====");
+        hewan k = new kucing();
+        k.setNama();
+        k.setSuara();
+        k.setBerat();
+        
+        System.out.println("====Hewan Singa====");
+        hewan s = new singa();
+        s.setNama();
+        s.setSuara();
+        s.setBerat();
+    }
+    
+}
